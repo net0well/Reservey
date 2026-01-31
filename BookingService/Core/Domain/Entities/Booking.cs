4cc4c61608c2d03aa745d9a94e666d04f65a04be
Wellington Neto <wellingtonalneto@gmail.com>
2026-01-31 20:15:31 -0300

    Add status machine

--- BookingService/Core/Domain/Entities/Booking.cs
+++ BookingService/Core/Domain/Entities/Booking.cs
@@ -1,9 +1,5 @@
 ﻿using Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Action = Domain.Enums.Action;
 
 namespace Domain.Entities
 {
@@ -18,6 +14,19 @@ namespace Domain.Entities
         public Status CurrentStatus
         {
             get { return this.Status; }
+        }
+
+        public void ChangeState(Action action)
+        {
+            this.Status = (this.Status, action) switch
+            {
+                (Status.Created,  Action.Pay)     => Status.Paid,
+                (Status.Created,  Action.Cancel)  => Status.Canceled,
+                (Status.Paid,     Action.Finish)  => Status.Finished,
+                (Status.Paid,     Action.Refound) => Status.Refounded,
+                (Status.Canceled, Action.Reopen)  => Status.Created,
+                _ => this.Status
+            };
         }
     }
 }
